75d29f09c011ad2c80867f06e753d37f0c47a8ff
= <me@alta.sh>
2021-01-27 10:10:49 +0000

    First commit

---
+++ Assets/Rocket.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    public float speed = 300.0f;
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (Input.GetKey("up"))
+            {
+                rigidbody.AddForce(transform.up * speed * Time.deltaTime);
+            };
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -1) * (speed / 1.5f) * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * (speed / 1.5f) * Time.deltaTime);
+        }
+    }
+}
